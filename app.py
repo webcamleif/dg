64fefe8922498c2b14330c51bdf8d6cd683c1fb0
webcamleif <henkehendrikx@gmail.com>
2023-10-11 22:28:25 +0000

    a

--- app.py
+++ app.py
@@ -86,6 +86,19 @@ def decline_invite_endpoint():
         return jsonify(success=True)
     return jsonify(success=False, message="Error declining invite.")
 
+@app.route('/accept_invite', methods=['POST'])
+def accept_invite_endpoint():
+    invite_id = request.form.get('invite_id')
+    invite = Invite.query.get(invite_id)
+    if invite and invite.receiver_id == current_user.id:
+        invite.status = "Accepted"
+        db.session.commit()
+        sender_sid = User.query.get(invite.sender_id).sid
+        if sender_sid:
+            socketio.emit('invite_accepted', {'invite_id': invite.id, 'friend_id': invite.receiver_id}, room=sender_sid)
+        return jsonify(success=True)
+    return jsonify(success=False, message="Error accepting invite.")
+
 @app.route('/send_invite', methods=['POST'])
 @login_required
 def send_invite_endpoint():
